b886966325e4ea1d1259edaa1ef77cb9b1aec92a
SpagPix <Spaghettipixel@gmail.com>
2019-06-02 12:53:03 +1000

    Added wand, box interactables and lerps

--- MagiciansApprentice-Project/MagiciansApprentice/Assets/_Project/Scripts/Wand.cs
+++ MagiciansApprentice-Project/MagiciansApprentice/Assets/_Project/Scripts/Wand.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Wand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform Pivot;
+
+    public LayerMask InteractableLayer; 
+    public float Range = 1000;
+
+    
+    private void Update()
+         
     {
-        
+        // If user presses fire button
+        // Shoot a ray 
+        // if we hit an object & it is Interactable     
+        // Do what we want to do
+       if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire Button Down");
+           
+      
+            if (Physics.Raycast(transform.position, transform.forward, out var hit, Range, InteractableLayer))
+            {
+                Debug.Log("Hit: " + hit.transform.name);
+                // TO DO : CHeck for interactable Component 
+                hit.transform.position = Pivot.transform.position; 
+
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrawGizmos()
     {
-        
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * Range);
     }
 }
+
